50c5912eb384b46ef68ff3b47502ad99e21a342c
Ted Fulk <teddyfulk@gmail.com>
2025-03-14 21:21:57 -0400

    update readme on suggest repo

--- Formula/suggest.rb
+++ Formula/suggest.rb
@@ -1,17 +1,17 @@
 class Suggest < Formula
   desc "A powerful command-line tool for interacting with AI models through various providers"
   homepage "https://github.com/tedfulk/suggest"
-  version "0.1.54"
+  version "0.1.55"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tedfulk/suggest/releases/download/v0.1.54/suggest-darwin-arm64.tar.gz"
-    sha256 "facd617bf8d9c9a57c2efd53514b522377b41e6511317f62d89722546851181c"
+    url "https://github.com/tedfulk/suggest/releases/download/v0.1.55/suggest-darwin-arm64.tar.gz"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tedfulk/suggest/releases/download/v0.1.54/suggest-darwin-amd64.tar.gz"
-    sha256 "5525ab5bd35009321ae2075584316dc46adda0f5d297e8ba36d1f57f1809cc88"
+    url "https://github.com/tedfulk/suggest/releases/download/v0.1.55/suggest-darwin-amd64.tar.gz"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tedfulk/suggest/releases/download/v0.1.54/suggest-linux-amd64.tar.gz"
-    sha256 "d4a892e45906e025170db8859b28d3c61e1be117008f5ba53ba9a848f03f81a7"
+    url "https://github.com/tedfulk/suggest/releases/download/v0.1.55/suggest-linux-amd64.tar.gz"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   end
 
   def install
