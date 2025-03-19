ebf3b82de2ce9effa02bf39ae1e33f4f7adbcb1e
Ted Fulk <teddyfulk@gmail.com>
2025-03-19 06:23:02 -0400

    fix: gemini api endpoint

--- Formula/suggest.rb
+++ Formula/suggest.rb
@@ -1,17 +1,17 @@
 class Suggest < Formula
   desc "A powerful command-line tool for interacting with AI models through various providers"
   homepage "https://github.com/tedfulk/suggest"
-  version "0.1.55"
+  version "0.1.56"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tedfulk/suggest/releases/download/v0.1.55/suggest-darwin-arm64.tar.gz"
-    sha256 "59a3a5cee52fef4af31aabb289a0a6164887fe221ee0c0bbd9e7bc6f107ee3e1"
+    url "https://github.com/tedfulk/suggest/releases/download/v0.1.56/suggest-darwin-arm64.tar.gz"
+    sha256 "68c9b51551202414761b8aec11c7ab2c0e4de8bd9f2157d22737a42ad41c38f5"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tedfulk/suggest/releases/download/v0.1.55/suggest-darwin-amd64.tar.gz"
-    sha256 "a769f37003853c2a340a5fc8d9e967e3bc3135b6791f069a937c03ce8c59e30a"
+    url "https://github.com/tedfulk/suggest/releases/download/v0.1.56/suggest-darwin-amd64.tar.gz"
+    sha256 "bd9b9aed53f21e14370bf04273e1f86e9d4ad8d8094e5cd9a1d5b57b8ddd8ec9"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tedfulk/suggest/releases/download/v0.1.55/suggest-linux-amd64.tar.gz"
-    sha256 "0042f18c1e29cbae22f0535efe5f97ae8e851a8178a122ca88805ae31f1e9f38"
+    url "https://github.com/tedfulk/suggest/releases/download/v0.1.56/suggest-linux-amd64.tar.gz"
+    sha256 "17ad6cbdb7967e49f0161f13702dae665a83f99912469eb7044a0c35373176a2"
   end
 
   def install
