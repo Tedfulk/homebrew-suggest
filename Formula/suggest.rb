1df50d1278de9303a27785f7c3d289b877f36945
Ted Fulk <teddyfulk@gmail.com>
2025-06-05 15:39:07 -0400

    fix url for sha for 0.1.57

--- Formula/suggest.rb
+++ Formula/suggest.rb
@@ -4,13 +4,13 @@ class Suggest < Formula
   version "0.1.56"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tedfulk/suggest/releases/download/v0.1.56/suggest-darwin-arm64.tar.gz"
+    url "https://github.com/tedfulk/suggest/releases/download/v0.1.57/suggest-darwin-arm64.tar.gz"
     sha256 "d25a2d5f74a66006d6be738f97a3a3509ca31653dc783e76d30aae7882972772"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tedfulk/suggest/releases/download/v0.1.56/suggest-darwin-amd64.tar.gz"
+    url "https://github.com/tedfulk/suggest/releases/download/v0.1.57/suggest-darwin-amd64.tar.gz"
     sha256 "f80afd643c3f454a6c1f7f78175ce45c5a34e3d3531886fa6483033858361749"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tedfulk/suggest/releases/download/v0.1.56/suggest-linux-amd64.tar.gz"
+    url "https://github.com/tedfulk/suggest/releases/download/v0.1.57/suggest-linux-amd64.tar.gz"
     sha256 "ce5ae79ad0ac08cfada86151af8dc233ac42f085d243d65605f1c1378db431b8"
   end
 
